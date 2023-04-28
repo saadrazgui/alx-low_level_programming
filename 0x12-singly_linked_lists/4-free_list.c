159781b9ab74fbea8023fc3b03740dbb23441a49
saadrazgui <saadrazgui8@gmail.com>
2023-04-28 06:14:39 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,13 +6,13 @@
  */
 void free_list(list_t *head)
 {
- list_t *temp;
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+list_t *temp;
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
 }
 
